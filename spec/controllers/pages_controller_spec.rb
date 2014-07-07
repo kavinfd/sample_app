d28558fca9f2aa06d749b3919c3776ab2e2332b8
kavinfd <kavin@freshdesk.com>
2014-07-07 13:12:05 +0530

    about page's spec added
    specification for about page
    
    Signed-off-by: kavinfd <kavin@freshdesk.com>

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -21,4 +21,11 @@ describe PagesController do
       response.should be_success
     end
   end
+
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+  end
 end
