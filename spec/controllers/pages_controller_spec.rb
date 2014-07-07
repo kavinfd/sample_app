7bb5f003a3505e56c6ed81bdcfaa624afad1de38
kavinfd <kavin@freshdesk.com>
2014-07-07 13:58:13 +0530

    static page to dynamic page change
    titles added
    
    Signed-off-by: kavinfd <kavin@freshdesk.com>

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,12 +1,28 @@
 require 'spec_helper'
 
 describe PagesController do
-
+  integrate_views
   #Delete these examples and add some real ones
   it "should use PagesController" do
     controller.should be_an_instance_of(PagesController)
   end
+  it "should have the right title" do
+    get 'home'
+    response.should have_tag("title",
+      "Ruby on Rails Tutorial Sample App | Home")
+  end
 
+  it "should have the right title" do
+    get 'contact'
+    response.should have_tag("title",
+      "Ruby on Rails Tutorial Sample App | Contact")
+  end
+
+  it "should have the right title" do
+    get 'about'
+    response.should have_tag("title",
+      "Ruby on Rails Tutorial Sample App | About")
+  end
 
   describe "GET 'home'" do
     it "should be successful" do
